6b509eba2475409b3a872dfb0f04eaef6bfd7472
Clara PALMIERI <cpalmier@e1r8p16.42.fr>
2019-02-21 17:07:27 +0100

    up

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpalmier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/28 13:35:31 by cpalmier          #+#    #+#             */
+/*   Updated: 2018/01/31 18:18:27 by cpalmier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# define BUFF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
+
+#endif
